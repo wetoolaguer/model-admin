2c67cc9f2a2b0afd5298a9202b91deba52a6cff8
wetoolaguer <wenceslaoolaguer@gmail.com>
2013-09-08 02:32:23 +0800

    Renames ModelObjectEvents to ModelAdminEvents.

--- model-admin/ModelObjectEvents.js
+++
@@ -1,8 +0,0 @@
-var ModelObjectEvents = {
-    ATTRIBUTE_ADDED : 'attributeAdded',
-    ATTRIBUTE_REMOVED : 'attributeRemoved',
-    RELATIONSHIP_ADDED : 'relationshipAdded',
-    RELATIONSHIP_REMOVED: 'relationshipRemoved'
-};
-
-module.exports = ModelObjectEvents;
